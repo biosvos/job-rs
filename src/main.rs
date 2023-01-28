4a9328cf77c4c8ccca6cb9599137224fa0344c0a
jinsu <biosvos@gmail.com>
2023-01-28 19:51:30 +0900

    base

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
-use reqwest::Error;
-
-#[tokio::main]
-async fn main() -> Result<(), Error> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Hello, world!");
-    let request_url = format!("https://career.programmers.co.kr/api/job_positions?min_salary=6000&order=recent&page=10&job_category_ids[]=1");
-    let response = reqwest::get(&request_url).await?;
-    let json = response.json::<serde_json::Value>().await?;
-    println!("{}", json["totalPages"]);
+    programmers(Option::from(1))?;
 
+    Ok(())
+}
+
+fn programmers(page: Option<i32>) -> Result<(), Box<dyn std::error::Error>> {
+    let url = "https://career.programmers.co.kr/api/job_positions";
+    let params = [
+        ("min_salary", "6000"),
+        ("order", "recent"),
+        ("page", &page.unwrap_or(1).to_string()),
+        ("job_category_ids[]", "1")
+    ];
+    let url = reqwest::Url::parse_with_params(url, &params)?;
+    let body = reqwest::blocking::get(url)?.text()?;
+    println!("{}", body);
     Ok(())
 }
