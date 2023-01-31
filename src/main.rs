e3868696a34cb9211260ed7b32a9d8b04b822a5a
jinsu <realwaterkim@datacommand.kr>
2023-01-31 12:54:21 +0900

    job 적용

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ extern crate glob;
 use glob::glob;
 use serde_json::Value;
 
+#[derive(Debug)]
 struct Company {
     id: u64,
     name: String,
@@ -15,6 +16,14 @@ struct Company {
     jobs: Vec<Job>,
 }
 
+impl Company {
+    fn add_job(&mut self, job: Job) -> Result<(), Box<dyn std::error::Error>> {
+        self.jobs.push(job);
+        Ok(())
+    }
+}
+
+#[derive(Debug)]
 struct Job {
     id: u64,
     title: String,
@@ -28,15 +37,26 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         let body = std::fs::read_to_string(entry?.as_path())?;
         let json: Value = serde_json::from_str(&body)?;
         for vec in json["jobPositions"].as_array().unwrap() {
-            println!("{}", vec["company"]["id"].as_u64().unwrap());
-            companies.insert(vec["company"]["id"].as_u64().unwrap(), Company{
-                id: 0,
-                name: "".to_string(),
-                address: "".to_string(),
-                jobs: vec![],
+            let key = vec["company"]["id"].as_u64().unwrap();
+            if !companies.contains_key(&key) {
+                companies.insert(key, Company {
+                    id: 0,
+                    name: "".to_string(),
+                    address: "".to_string(),
+                    jobs: Vec::new(),
+                });
+            }
+            let url = format!("https://career.programmers.co.kr/job_positions/{}", vec["id"].as_u64().unwrap());
+            companies.entry(key).and_modify(|company| {
+                company.add_job(Job {
+                    id: vec["id"].as_u64().unwrap(),
+                    title: vec["title"].to_string(),
+                    url,
+                }).unwrap();
             });
         }
     }
+    println!("{:?}", companies);
 
     Ok(())
 }
